94f4cd9276fb8bbcfd7a2d7f99da6fb2f246f0c2
olgakahraman <ol.khrmn@gmail.com>
2024-04-25 13:24:58 -0400

    improved the getFeedHtml function with forEach method

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ tweetBtn.addEventListener('click', function(){
 function getFeedHtml() {
   let feedHtml = ``;
 
-  for (let tweet of tweetsData) {
+  tweetsData.forEach(function(tweet) {
     feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -32,7 +32,7 @@ function getFeedHtml() {
         </div>            
     </div>
 </div>`;
-  }
+  })
   return feedHtml;
 
 }
