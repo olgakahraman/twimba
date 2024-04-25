ed37796974c6a993f8972ede32b025cff6c5fce5
olgakahraman <ol.khrmn@gmail.com>
2024-04-25 14:34:28 -0400

    set the data attributes for icons to get access to them in js

--- index.js
+++ index.js
@@ -20,15 +20,18 @@ function getFeedHtml() {
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
-                <i class="fa-solid fa-comment-dots"></i>
+                <i class="fa-solid fa-comment-dots"
+                data-reply="${tweet.uuid}"></i>
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-heart"></i>
+                <i class="fa-solid fa-heart"
+                data-like="${tweet.uuid}"></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-retweet"></i>
+                <i class="fa-solid fa-retweet"
+                data-retweet="${tweet.uuid}"></i>
                     ${tweet.retweets}
                 </span>
             </div>   
