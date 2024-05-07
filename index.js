23621b9c9bdf43bf7e65c59642c34e1417ed3a59
olgakahraman <ol.khrmn@gmail.com>
2024-05-07 14:34:59 -0400

    rendered a new tweet with .unshift() method which adds an element im the beginning of array

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function handleReplyClick(replyId) {
 }
 
 function handleTweetBtnClick() {
-  console.log({
+  tweetsData.unshift({
         handle: `@Scrimba`,
         profilePic: `images/scrimbalogo.png`,
         likes: 0,
@@ -64,9 +64,10 @@ function handleTweetBtnClick() {
         replies: [],
         isLiked: false,
         isRetweeted: false,
-        uuid: uuidv4(),
-    },)
-}
+        uuid: uuidv4()
+    })
+    render();
+};
 
 
 
