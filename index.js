57598cdc54d033ea4542b45932cbf9f65447afc4
olgakahraman <ol.khrmn@gmail.com>
2024-05-07 11:42:30 -0400

    built a new Tweet object

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { tweetsData } from "./data.js";
+  import { v4 as uuidv4 } from "https://jspm.dev/uuid"; 
 
 const tweetInput = document.getElementById("tweet-input");
 
@@ -54,8 +55,20 @@ function handleReplyClick(replyId) {
 }
 
 function handleTweetBtnClick() {
-  console.log(tweetInput.value);
+  console.log({
+        handle: `@Scrimba`,
+        profilePic: `images/scrimbalogo.png`,
+        likes: 0,
+        retweets: 0,
+        tweetText: tweetInput.value,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: uuidv4(),
+    },)
 }
+
+
 
 function getFeedHtml() {
   let feedHtml = ``;
